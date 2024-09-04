9d28b88bb2ae02562a9749384d681b2f5931f4a7
chiexplores <contact@chihuynh.me>
2024-09-03 18:25:31 -1000

    feature: Add skate image, button press

--- skate-anatomy/App.js
+++ skate-anatomy/App.js
@@ -1,20 +1,55 @@
+import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, Pressable } from 'react-native';
+import SkateboardSVG from './assets/skate.svg';
+import Svg, { Circle } from 'react-native-svg';
+
 
 export default function App() {
+  const [timesPressed, setTimesPressed] = useState(0);
+
+  let textLog = '';
+  if (timesPressed > 1) {
+    textLog = timesPressed + 'x onPress';
+  } else if (timesPressed > 0) {
+    textLog = 'onPress';
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up HELLO </Text>
       <StatusBar style="auto" />
+      {/* <Circle cx="50" cy="50" r="50" stroke="purple" strokeWidth=".5" fill="violet" /> */}
+      {/* <Circle cx="200" cy="500" r="50" stroke="red" strokeWidth=".5" fill="yellow" /> */}
+      <Image source={require('./assets/skateboard.jpg')} />
+      <Pressable
+        onPress={() => {
+          setTimesPressed(current => current + 1);
+        }}
+        style={({ pressed }) => [
+          {
+            backgroundColor: pressed ? 'rgb(210, 230, 255)' : 'white',
+          },
+          styles.wrapperCustom,
+        ]}>
+        {({ pressed }) => (
+          <Text style={styles.text}>{pressed ? 'Pressed!' : 'Press Me'}</Text>
+        )}
+      </Pressable>
+
+
+
+
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    display: "grid",
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "blue",
+    height: "100%",
   },
 });
---
+++ skate-anatomy/SkateBoard.jsx
@@ -0,0 +1,23 @@
+import { StyleSheet, Text, View } from 'react-native';
+import ReactDOM from 'react-dom';
+import SkateboardSVG from './assets/skate.svg';
+
+export default function Skateboard() {
+  return (
+    <View style={styles.container}>
+      <h1>Skateboard Illustration</h1>
+    </View>
+  );
+}
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
+
+
+
